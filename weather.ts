cf3ecaced1f670d5c7627caf48de5768d9a47806
Y Q N O <79216939+Yqno@users.noreply.github.com>
2023-04-01 12:16:43 +0200

    Update weather.ts

--- weather.ts
+++ weather.ts
@@ -1,28 +1,26 @@
 interface WetterDaten {
     temperatur: number;
     windgeschwindigkeit: number;
-    feuchtigkeit:number;
-    beschreibung:string;
-
+    feuchtigkeit: number;
+    beschreibung: string;
 }
 
 function getWeather(stadt: string): WetterDaten {
     // Normal sollte man eine API Nutzen doch ich nutze hier Lokale Daten
-    const data = {
+    const data: WetterDaten = {
         temperatur: 18,
         windgeschwindigkeit: 10,
         feuchtigkeit: 70,
         beschreibung: "Bewölkt",
-      };
-      return data;
+    };
+    return data;
 }
-
 
 const city = "Berlin";
 const wetterdaten = getWeather(city);
 
 console.log(`Das Wetter in ${city}:`);
-console.log(`Temperatur: ${WetterDaten.temperatur}°C`);
-console.log(`Windgeschwindigkeit: ${WetterDaten.windgeschwindigkeit} km/h`);
-console.log(`Luftfeuchtigkeit: ${weatherData.feuchtigkeit}%`);
-console.log(`Beschreibung: ${weatherData.beschreibung}`);
+console.log(`Temperatur: ${wetterdaten.temperatur}°C`);
+console.log(`Windgeschwindigkeit: ${wetterdaten.windgeschwindigkeit} km/h`);
+console.log(`Luftfeuchtigkeit: ${wetterdaten.feuchtigkeit}%`);
+console.log(`Beschreibung: ${wetterdaten.beschreibung}`);
